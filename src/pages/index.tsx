97e04cfad7ee8c88abce566b05af4011e6924d71
armand <armand.salle@gmail.com>
2023-09-14 20:17:00 +0200

    feat(home-page): change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <main className="flex flex-col items-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight  sm:text-[5rem]">
-            Create T3 App
+            Pet card
           </h1>
 
           <div className="flex flex-col items-center gap-2">
